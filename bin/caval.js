689e4f6f8a60964a91a5498d2d1cfd613bf73550
fluiddevelopmentnet <ruhljeremias@gmail.com>
2023-02-21 16:28:09 +0100

    arg pipe init

--- bin/caval.js
+++ bin/caval.js
@@ -9,12 +9,13 @@ try {
 
 var path = require('path');
 var fs   = require('fs');
+const process = require('process')
 
 
 if (fs.existsSync(path.join(__dirname, '../lib/cavalir'))) {
-    require('../lib/cavalir/command').run();
+    require('../lib/cavalir/command')(process.argv);
 }else if (fs.existsSync(path.join(process.cwd(), 'node_modules/cavalir/lib/cavalir'))){
-    require('node_modules/cavalir/lib/cavalir/command').run();
+    require('node_modules/cavalir/lib/cavalir/command')(process.argv);
 }else {
     console.error('module not found! (by the CMD)');
     process.exit(1);
