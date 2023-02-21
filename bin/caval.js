ce8b1c2a114cb5e1ad78b26144321d05cafc907f
fluiddevelopmentnet <ruhljeremias@gmail.com>
2023-02-21 15:29:03 +0100

    link lib directory + start command

--- bin/caval.js
+++ bin/caval.js
@@ -1,2 +1,26 @@
 #!/usr/bin/env node
 
+try {
+  new Function('var {a} = {a: 1}')();
+} catch (error) {
+  console.error('Node does not work.');
+  process.exit(1);
+}
+
+var path = require('path');
+var fs   = require('fs');
+
+var potentialPaths = [
+  path.join(process.cwd(), 'node_modules/cavalir/lib/cavalir'),
+  path.join(__dirname, '../lib/cavalir')
+];
+
+
+if (fs.existsSync(path.join(__dirname, '../lib/cavalir'))) {
+    require('../lib/cavalir/command').run();
+}else if (fs.existsSync(path.join(process.cwd(), 'node_modules/cavalir/lib/cavalir'))){
+    require('node_modules/cavalir/lib/cavalir/command').run();
+}else {
+    console.error('module not found! (by the CMD)');
+    process.exit(1);
+}
